91c542b3c134128016d84f01b7693b7ba3f1dc47
Hamster <hamster620@gmail.com>
2021-06-07 17:13:51 +0800

    Define Log and LogLevel.

---
+++ ULogViewer/BaseLog.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace CarinaStudio.ULogViewer
+{
+	/// <summary>
+	/// Base implementation of <see cref="ILog"/>.
+	/// </summary>
+	abstract class BaseLog : ILog
+	{
+		/// <summary>
+		/// Initialize new <see cref="BaseLog"/> instance.
+		/// </summary>
+		protected BaseLog()
+		{ }
+
+
+		/// <summary>
+		/// Get level.
+		/// </summary>
+		public abstract LogLevel Level { get; }
+
+
+		/// <summary>
+		/// Get message.
+		/// </summary>
+		public virtual string? Message { get; }
+
+
+		/// <summary>
+		/// Get ID of process which generates log.
+		/// </summary>
+		public virtual int? ProcessId { get; }
+
+
+		/// <summary>
+		/// Get name of process which generates log.
+		/// </summary>
+		public virtual string? ProcessName { get; }
+
+
+		/// <summary>
+		/// Get name of source which generates log.
+		/// </summary>
+		public virtual string? SourceName { get; }
+
+
+		/// <summary>
+		/// Get ID of thread which generates log.
+		/// </summary>
+		public virtual int? ThreadId { get; }
+
+
+		/// <summary>
+		/// Get name of thread which generates log.
+		/// </summary>
+		public virtual string? ThreadName { get; }
+
+
+		/// <summary>
+		/// Get timestamp.
+		/// </summary>
+		public virtual DateTime? Timestamp { get; }
+
+
+		/// <summary>
+		/// Get ID of user which generates log.
+		/// </summary>
+		public virtual string? UserId { get; }
+
+
+		/// <summary>
+		/// Get name of user which generates log.
+		/// </summary>
+		public virtual string? UserName { get; }
+	}
+}
---
+++ ULogViewer/LogLevel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CarinaStudio.ULogViewer
+{
+	/// <summary>
+	/// Level of log.
+	/// </summary>
+	enum LogLevel
+	{
+		/// <summary>
+		/// Undefined.
+		/// </summary>
+		Undefined,
+		/// <summary>
+		/// Verbose.
+		/// </summary>
+		Verbose,
+		/// <summary>
+		/// Debug.
+		/// </summary>
+		Debug,
+		/// <summary>
+		/// Info.
+		/// </summary>
+		Info,
+		/// <summary>
+		/// Warn.
+		/// </summary>
+		Warn,
+		/// <summary>
+		/// Error.
+		/// </summary>
+		Error,
+		/// <summary>
+		/// Fatal.
+		/// </summary>
+		Fatal,
+	}
+}
