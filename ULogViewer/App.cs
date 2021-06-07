83762cbeaefe21ffacc6bc92f7c2e98a407d9983
Hamster <hamster620@gmail.com>
2021-06-07 14:21:36 +0800

    Create empty project.

---
+++ ULogViewer/App.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia;
+using System;
+using System.Collections.Generic;
+
+namespace CarinaStudio.ULogViewer
+{
+	/// <summary>
+	/// Application.
+	/// </summary>
+	class App : Application
+	{
+		// Program entry.
+		static void Main(string[] args)
+		{ }
+	}
+}
