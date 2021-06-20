0d5facb5555a7e47652050da51846e95547eea6a
Hamster <hamster620@gmail.com>
2021-06-20 19:30:13 +0800

    Base implementations of tests.

---
+++ ULogViewer.Tests/Logs/DataSources/BaseLogDataSourceTests.cs
@@ -0,0 +1,14 @@
+﻿using CarinaStudio.Threading;
+using NUnit.Framework;
+using System;
+
+namespace CarinaStudio.ULogViewer.Logs.DataSources
+{
+	/// <summary>
+	/// Base implementation of tests of <see cref="ILogDataSource"/>.
+	/// </summary>
+	abstract class BaseLogDataSourceTests<TProvider, TSource> : AppBasedTests where TProvider : ILogDataSourceProvider where TSource : ILogDataSource
+	{
+
+	}
+}
---
+++ ULogViewer.Tests/TestApp.cs
@@ -0,0 +1,42 @@
+﻿using Avalonia;
+using Avalonia.ReactiveUI;
+using CarinaStudio.Threading;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace CarinaStudio.ULogViewer
+{
+	/// <summary>
+	/// <see cref="App"/> for testing.
+	/// </summary>
+	class TestApp : App
+	{
+		// Fields.
+		static volatile bool isSetupCompleted;
+		static volatile SingleThreadSynchronizationContext? syncContext;
+
+
+		// Avalonia configuration, don't remove; also used by visual designer.
+		static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<TestApp>()
+			.UsePlatformDetect()
+			.UseReactiveUI()
+			.LogToTrace();
+
+
+		/// <summary>
+		/// Setup <see cref="TestApp"/> instance.
+		/// </summary>
+		[MethodImpl(MethodImplOptions.Synchronized)]
+		public static void Setup()
+		{
+			if (isSetupCompleted)
+				return;
+			isSetupCompleted = true;
+			syncContext = new SingleThreadSynchronizationContext();
+			syncContext.Post(() =>
+			{
+				BuildAvaloniaApp().SetupWithoutStarting();
+			});
+		}
+	}
+}
