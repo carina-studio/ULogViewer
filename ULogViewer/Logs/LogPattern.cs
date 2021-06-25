61f87e6b0e14a89459aba5f9e7014c3eecbbb0d7
Hamster <hamster620@gmail.com>
2021-06-25 15:20:42 +0800

    Add test case of reading logs from file by LogReader.

--- ULogViewer/Logs/LogPattern.cs
+++ ULogViewer/Logs/LogPattern.cs
@@ -24,6 +24,20 @@ namespace CarinaStudio.ULogViewer.Logs
 
 
 		/// <summary>
+		/// Initialize new <see cref="LogPattern"/> instance.
+		/// </summary>
+		/// <param name="regex">Regular expression for parsing log data.</param>
+		/// <param name="isRepeatable">Whether the pattern is repeatable or not when parsing log data.</param>
+		/// <param name="isSkippable">Whether the pattern can be skipped or not when parsing log data.</param>
+		public LogPattern(Regex regex, bool isRepeatable, bool isSkippable)
+		{
+			this.IsRepeatable = isRepeatable;
+			this.IsSkippable = isSkippable;
+			this.Regex = regex;
+		}
+
+
+		/// <summary>
 		/// Get whether the pattern is repeatable or not when parsing log data.
 		/// </summary>
 		public bool IsRepeatable { get; }
