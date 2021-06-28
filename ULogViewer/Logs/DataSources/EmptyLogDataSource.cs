83563879df6b63fa69c02ee0db7bf79ef23cb684
Hamster <hamster620@gmail.com>
2021-06-28 20:35:46 +0800

    Add empty implementation of ILogDataSource and its provider.

---
+++ ULogViewer/Logs/DataSources/EmptyLogDataSource.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace CarinaStudio.ULogViewer.Logs.DataSources
+{
+	/// <summary>
+	/// Empty implementation of <see cref="ILogDataSource"/>.
+	/// </summary>
+	class EmptyLogDataSource : BaseLogDataSource
+	{
+		/// <summary>
+		/// Initialize new <see cref="EmptyLogDataSource"/> instance.
+		/// </summary>
+		/// <param name="provider">Provider.</param>
+		/// <param name="options">Options.</param>
+		public EmptyLogDataSource(EmptyLogDataSourceProvider provider, LogDataSourceOptions options) : base(provider, options)
+		{ }
+
+
+		// Open reader.
+		protected override LogDataSourceState OpenReaderCore(out TextReader? reader)
+		{
+			reader = null;
+			return LogDataSourceState.UnclassifiedError;
+		}
+
+
+		// Prepare.
+		protected override LogDataSourceState PrepareCore() => LogDataSourceState.UnclassifiedError;
+	}
+}
---
+++ ULogViewer/Logs/DataSources/EmptyLogDataSourceProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CarinaStudio.ULogViewer.Logs.DataSources
+{
+	/// <summary>
+	/// <see cref="ILogDataSourceProvider"/> for <see cref="EmptyLogDataSource"/>.
+	/// </summary>
+	class EmptyLogDataSourceProvider : BaseLogDataSourceProvider
+	{
+		/// <summary>
+		/// Initialize new <see cref="EmptyLogDataSourceProvider"/> instance.
+		/// </summary>
+		/// <param name="app">Application.</param>
+		public EmptyLogDataSourceProvider(IApplication app) : base(app)
+		{ }
+
+
+		// Implementations.
+		protected override ILogDataSource CreateSourceCore(LogDataSourceOptions options) => new EmptyLogDataSource(this, options);
+		public override string Name => "Empty";
+		public override UnderlyingLogDataSource UnderlyingSource => UnderlyingLogDataSource.Undefined;
+	}
+}
