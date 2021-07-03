bcbf76204f07b2fca11949edb4ca7e1caa47d185
Hamster <hamster620@gmail.com>
2021-07-03 10:10:07 +0800

    Add temporary test code in MainWindow.

--- ULogViewer/MainWindow.axaml.cs
+++ ULogViewer/MainWindow.axaml.cs
@@ -12,16 +12,32 @@ namespace CarinaStudio.ULogViewer
 	/// </summary>
 	partial class MainWindow : BaseWindow
 	{
+		ViewModels.Session session;
+
+
 		/// <summary>
 		/// Initialize new <see cref="MainWindow"/> instance.
 		/// </summary>
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			var sessionView = this.FindControl<SessionView>("sessionView").AsNonNull();
+			session = new ViewModels.Session(App.Current);
+
+			sessionView.DataContext = session;
 		}
 
 
 		// Initialize Avalonia components.
 		private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+
+
+		protected override void OnOpened(EventArgs e)
+		{
+			base.OnOpened(e);
+
+			this.SynchronizationContext.PostDelayed(() => session.SetLogProfileCommand.Execute(Logs.Profiles.LogProfiles.All[0]), 1000);
+		}
 	}
 }
