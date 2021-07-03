e3f7869978701b94bcce30e73838165ff53e25d8
Hamster <hamster620@gmail.com>
2021-07-03 20:19:16 +0800

    Generate log header views according to Session.DisplayLogProperties.

---
+++ ULogViewer/Controls/ControlExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Avalonia.Controls;
+using System;
+
+namespace CarinaStudio.ULogViewer.Controls
+{
+	/// <summary>
+	/// Extensions for <see cref="Control"/>.
+	/// </summary>
+	static class ControlExtensions
+	{
+		/// <summary>
+		/// Find child control by name.
+		/// </summary>
+		/// <typeparam name="T">Type of child control.</typeparam>
+		/// <param name="control">Parent control.</param>
+		/// <param name="name">Name of child control.</param>
+		/// <returns>Child control with specific name or null if no child control found.</returns>
+		public static T? FindChildControl<T>(this IControl control, string name) where T : class, IControl
+		{
+			if (control is ContentControl contentControl)
+			{
+				var child = contentControl.Content as IControl;
+				if (child == null)
+					return null;
+				if (child.Name == name)
+					return (T)child;
+				return child.FindChildControl<T>(name);
+			}
+			else if (control is Decorator decorator)
+			{
+				var child = decorator.Child;
+				if (child.Name == name)
+					return (T)child;
+				return child.FindChildControl<T>(name);
+			}
+			else if (control is Panel panel)
+			{
+				foreach (var child in panel.Children)
+				{
+					if (child.Name == name)
+						return (T)child;
+				}
+				foreach (var child in panel.Children)
+				{
+					var result = child.FindChildControl<T>(name);
+					if (result != null)
+						return result;
+				}
+			}
+			return null;
+		}
+	}
+}
--- ULogViewer/Controls/SessionView.axaml.cs
+++ ULogViewer/Controls/SessionView.axaml.cs
@@ -1,12 +1,17 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Layout;
 using Avalonia.LogicalTree;
 using Avalonia.Markup.Xaml;
+using Avalonia.Markup.Xaml.Templates;
 using Avalonia.VisualTree;
+using CarinaStudio.Collections;
 using CarinaStudio.ULogViewer.ViewModels;
 using Microsoft.Extensions.Logging;
 using ReactiveUI;
 using System;
+using System.ComponentModel;
+using System.Linq;
 using System.Windows.Input;
 
 namespace CarinaStudio.ULogViewer.Controls
@@ -42,23 +47,91 @@ namespace CarinaStudio.ULogViewer.Controls
 		// Attach to session.
 		void AttachToSession(Session session)
 		{
+			// add event handler
+			session.PropertyChanged += this.OnSessionPropertyChanged;
+
 			// attach to command
 			session.SetWorkingDirectoryCommand.CanExecuteChanged += this.OnSessionCommandCanExecuteChanged;
 			this.canSetWorkingDirectory.Update(session.SetWorkingDirectoryCommand.CanExecute(null));
+
+			// update UI
+			this.OnDisplayLogPropertiesChanged();
 		}
 
 
 		// Detach from session.
 		void DetachFromSession(Session session)
 		{
+			// remove event handler
+			session.PropertyChanged -= this.OnSessionPropertyChanged;
+
 			// detach from commands
 			session.SetWorkingDirectoryCommand.CanExecuteChanged -= this.OnSessionCommandCanExecuteChanged;
 			this.canSetWorkingDirectory.Update(false);
+
+			// update UI
+			this.OnDisplayLogPropertiesChanged();
 		}
 
 
 		// Initialize Avalonia components.
 		private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+
+
+		// Called when display log properties changed.
+		void OnDisplayLogPropertiesChanged()
+		{
+			// clear headers
+			foreach (var control in this.logHeaderGrid.Children)
+				control.DataContext = null;
+			this.logHeaderGrid.Children.Clear();
+			this.logHeaderGrid.ColumnDefinitions.Clear();
+
+			// get display log properties
+			var logProperties = (this.DataContext as Session)?.DisplayLogProperties;
+			if (logProperties == null || logProperties.IsEmpty())
+				return;
+
+			// build headers
+			var headerTemplate = (DataTemplate)this.DataTemplates.First(it => it is DataTemplate dt && dt.DataType == typeof(DisplayableLogProperty));
+			for (int i = 0, count = logProperties.Count; i < count; ++i)
+			{
+				// define column
+				var logProperty = logProperties[i];
+				var width = logProperty.Width;
+				var columnWidth = width.Let(width =>
+				{
+					if (width == null)
+						return new GridLength(1, GridUnitType.Star);
+					return new GridLength(0, GridUnitType.Auto);
+				});
+				var column = new ColumnDefinition(columnWidth).Also(it =>
+				{
+					it.SharedSizeGroup = logProperty.Name;
+				});
+				this.logHeaderGrid.ColumnDefinitions.Add(column);
+
+				// create header view
+				var headerView = ((Border)headerTemplate.Build(logProperty)).Also(it =>
+				{
+					it.DataContext = logProperty;
+					if (i == 0)
+						it.BorderThickness = new Thickness();
+					if (width == null)
+						it.HorizontalAlignment = HorizontalAlignment.Stretch;
+					else if (width > 0)
+					{
+						it.FindChildControl<TextBlock>("widthControlTextBlock").AsNonNull().Text = new string(new char[width.Value].Also(it =>
+						{
+							for (var j = it.Length - 1; j >= 0; --j)
+								it[j] = ' ';
+						}));
+					}
+					Grid.SetColumn(it, i);
+				});
+				this.logHeaderGrid.Children.Add(headerView);
+			}
+		}
 
 
 		// Property changed.
@@ -89,6 +162,18 @@ namespace CarinaStudio.ULogViewer.Controls
 				}
 				else
 					this.canSetWorkingDirectory.Update(false);
+			}
+		}
+
+
+		// Called when property of session has been changed.
+		void OnSessionPropertyChanged(object? sender, PropertyChangedEventArgs e)
+		{
+			switch (e.PropertyName)
+			{
+				case nameof(Session.DisplayLogProperties):
+					this.OnDisplayLogPropertiesChanged();
+					break;
 			}
 		}
 
