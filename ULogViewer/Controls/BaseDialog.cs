f589158b46f80da38439a99a8318ab0c56698055
Hamster <hamster620@gmail.com>
2021-07-04 17:57:40 +0800

    Add BaseDialog for dialogs.

---
+++ ULogViewer/Controls/BaseDialog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CarinaStudio.ULogViewer.Controls
+{
+	/// <summary>
+	/// Base class for dialog in ULogViewer.
+	/// </summary>
+	abstract class BaseDialog : BaseWindow
+	{
+		// Fields.
+		BaseWindow? ownerWindow;
+
+
+		// Called when closed.
+		protected override void OnClosed(EventArgs e)
+		{
+			this.ownerWindow?.OnDialogClosed(this);
+			this.ownerWindow = null;
+			base.OnClosed(e);
+		}
+
+
+		// Called when opened.
+		protected override void OnOpened(EventArgs e)
+		{
+			base.OnOpened(e);
+			this.ownerWindow = (this.Owner as BaseWindow)?.Also(it => it.OnDialogOpened(this));
+		}
+	}
+}
