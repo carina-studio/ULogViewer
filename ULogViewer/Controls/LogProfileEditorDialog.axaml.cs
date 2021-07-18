696714536e04b397a9887b4b347a9c7a087f148b
Hamster <hamster620@gmail.com>
2021-07-18 17:17:43 +0800

    Implementation of LogProfileEditorDialog (2/n).

--- ULogViewer/Controls/LogProfileEditorDialog.axaml.cs
+++ ULogViewer/Controls/LogProfileEditorDialog.axaml.cs
@@ -5,9 +5,11 @@ using Avalonia.Markup.Xaml;
 using CarinaStudio.Collections;
 using CarinaStudio.Threading;
 using CarinaStudio.ULogViewer.Converters;
+using CarinaStudio.ULogViewer.Logs.DataSources;
 using CarinaStudio.ULogViewer.Logs.Profiles;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CarinaStudio.ULogViewer.Controls
 {
@@ -22,9 +24,17 @@ namespace CarinaStudio.ULogViewer.Controls
 		public static readonly IValueConverter LogProfileNameConverter = new EnumConverter<LogProfileIcon>(App.Current);
 
 
+		// Static fields.
+		static readonly AvaloniaProperty<bool> IsValidDataSourceOptionsProperty = AvaloniaProperty.Register<LogProfileEditorDialog, bool>(nameof(IsValidDataSourceOptions), true);
+		static readonly AvaloniaProperty<UnderlyingLogDataSource> UnderlyingDataSourceProperty = AvaloniaProperty.Register<LogProfileEditorDialog, UnderlyingLogDataSource>(nameof(UnderlyingDataSource), UnderlyingLogDataSource.Undefined);
+
+
 		// Fields.
+		LogDataSourceOptions dataSourceOptions;
+		readonly ComboBox dataSourceProviderComboBox;
 		readonly ComboBox iconComboBox;
 		readonly TextBox nameTextBox;
+		readonly ToggleSwitch workingDirNeededSwitch;
 
 
 		/// <summary>
@@ -33,13 +43,19 @@ namespace CarinaStudio.ULogViewer.Controls
 		public LogProfileEditorDialog()
 		{
 			InitializeComponent();
+			this.dataSourceProviderComboBox = this.FindControl<ComboBox>("dataSourceProviderComboBox").AsNonNull();
 			this.iconComboBox = this.FindControl<ComboBox>("iconComboBox").AsNonNull();
 			this.nameTextBox = this.FindControl<TextBox>("nameTextBox").AsNonNull();
+			this.workingDirNeededSwitch = this.FindControl<ToggleSwitch>("nameworkingDirNeededSwitchTextBox").AsNonNull();
 		}
 
 
 		// Initialize.
 		private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+
+
+		// Check whether log data source options is valid or not.
+		bool IsValidDataSourceOptions { get => this.GetValue<bool>(IsValidDataSourceOptionsProperty); }
 
 
 		/// <summary>
@@ -52,12 +68,23 @@ namespace CarinaStudio.ULogViewer.Controls
 		IList<LogProfileIcon> LogProfileIcons { get; } = (LogProfileIcon[])Enum.GetValues(typeof(LogProfileIcon));
 
 
+		// Called when selection of data source provider changed.
+		void OnDataSourceProviderComboBoxSelectionChanged(object? sender, SelectionChangedEventArgs e)
+		{
+			var dataSourceProvider = (this.dataSourceProviderComboBox.SelectedItem as ILogDataSourceProvider);
+			this.SetValue<UnderlyingLogDataSource>(UnderlyingDataSourceProperty, dataSourceProvider?.UnderlyingSource ?? UnderlyingLogDataSource.Undefined);
+			this.InvalidateInput();
+		}
+
+
 		// Called when property of editor control has been changed.
 		void OnEditorControlPropertyChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
 		{
 			var property = e.Property;
 			if (property == TextBox.TextProperty)
+			{
 				this.InvalidateInput();
+			}
 		}
 
 
@@ -75,13 +102,17 @@ namespace CarinaStudio.ULogViewer.Controls
 			if (profile == null)
 			{
 				this.Title = this.Application.GetString("LogProfileEditorDialog.Title.Create");
+				this.dataSourceProviderComboBox.SelectedItem = LogDataSourceProviders.All.FirstOrDefault(it => it is FileLogDataSourceProvider);
 				this.iconComboBox.SelectedItem = LogProfileIcon.File;
 			}
 			else if (!profile.IsBuiltIn)
 			{
 				this.Title = this.Application.GetString("LogProfileEditorDialog.Title.Edit");
-				this.nameTextBox.Text = profile.Name;
+				this.dataSourceOptions = profile.DataSourceOptions;
+				this.dataSourceProviderComboBox.SelectedItem = profile.DataSourceProvider;
 				this.iconComboBox.SelectedItem = profile.Icon;
+				this.nameTextBox.Text = profile.Name;
+				this.workingDirNeededSwitch.IsChecked = profile.IsWorkingDirectoryNeeded;
 			}
 			else
 				this.SynchronizationContext.Post(this.Close);
@@ -97,6 +128,17 @@ namespace CarinaStudio.ULogViewer.Controls
 			if (!base.OnValidateInput())
 				return false;
 
+			// check data source and options
+			var dataSourceProvider = (this.dataSourceProviderComboBox.SelectedItem as ILogDataSourceProvider);
+			if (dataSourceProvider == null)
+				return false;
+			if (dataSourceProvider.UnderlyingSource == UnderlyingLogDataSource.StandardOutput && string.IsNullOrWhiteSpace(this.dataSourceOptions.Command))
+			{
+				this.SetValue<bool>(IsValidDataSourceOptionsProperty, false);
+				return false;
+			}
+			this.SetValue<bool>(IsValidDataSourceOptionsProperty, true);
+
 			// check name
 			if (string.IsNullOrEmpty(this.nameTextBox.Text))
 				return false;
@@ -104,5 +146,28 @@ namespace CarinaStudio.ULogViewer.Controls
 			// ok
 			return true;
 		}
+
+
+		// Set log data source options.
+		async void SetDataSourceOptions()
+		{
+			var dataSourceProvider = (this.dataSourceProviderComboBox.SelectedItem as ILogDataSourceProvider);
+			if (dataSourceProvider == null)
+				return;
+			var options = await new LogDataSourceOptionsDialog()
+			{
+				Options = this.dataSourceOptions,
+				UnderlyingLogDataSource = dataSourceProvider.UnderlyingSource,
+			}.ShowDialog<LogDataSourceOptions?>(this);
+			if (options != null)
+			{
+				this.dataSourceOptions = options.Value;
+				this.InvalidateInput();
+			}
+		}
+
+
+		// Get underlying type of log data source.
+		UnderlyingLogDataSource UnderlyingDataSource { get => this.GetValue<UnderlyingLogDataSource>(UnderlyingDataSourceProperty); }
 	}
 }
