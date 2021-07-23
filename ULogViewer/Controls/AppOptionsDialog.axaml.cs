bbb147b5cdb8b5ff7f8cb519f5ae8d439be0434d
Hamster <hamster620@gmail.com>
2021-07-23 16:01:30 +0800

    Make sure that AppOptionsDialog will refresh after changing culture or theme mode.

--- ULogViewer/Controls/AppOptionsDialog.axaml.cs
+++ ULogViewer/Controls/AppOptionsDialog.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using CarinaStudio.Configuration;
+using CarinaStudio.Threading;
 using CarinaStudio.ULogViewer.ViewModels;
 using System;
 
@@ -11,6 +13,10 @@ namespace CarinaStudio.ULogViewer.Controls
 	/// </summary>
 	partial class AppOptionsDialog : BaseDialog
 	{
+		// Fields.
+		readonly ScheduledAction refreshDataContextAction;
+
+
 		/// <summary>
 		/// Initialize new <see cref="AppOptionsDialog"/> instance.
 		/// </summary>
@@ -18,6 +24,14 @@ namespace CarinaStudio.ULogViewer.Controls
 		{
 			this.DataContext = new AppOptions(App.Current);
 			InitializeComponent();
+			App.Current.StringsUpdated += this.OnApplicationStringsUpdated;
+			this.Settings.SettingChanged += this.OnSettingChanged;
+			this.refreshDataContextAction = new ScheduledAction(() =>
+			{
+				var dataContext = this.DataContext;
+				this.DataContext = null;
+				this.DataContext = dataContext;
+			});
 		}
 
 		
@@ -25,9 +39,19 @@ namespace CarinaStudio.ULogViewer.Controls
 		private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
 
 
+		// Called when strings updated.
+		void OnApplicationStringsUpdated(object? sender, EventArgs e)
+		{
+			this.refreshDataContextAction.Schedule();
+		}
+
+
 		// Called when closed.
 		protected override void OnClosed(EventArgs e)
 		{
+			App.Current.StringsUpdated -= this.OnApplicationStringsUpdated;
+			this.Settings.SettingChanged -= this.OnSettingChanged;
+			this.refreshDataContextAction.Cancel();
 			(this.DataContext as AppOptions)?.Dispose();
 			this.DataContext = null;
 			base.OnClosed(e);
@@ -36,5 +60,13 @@ namespace CarinaStudio.ULogViewer.Controls
 
 		// Generate result.
 		protected override object? OnGenerateResult() => null;
+
+
+		// Called when setting changed.
+		void OnSettingChanged(object? sender, SettingChangedEventArgs e)
+		{
+			if (e.Key == Settings.ThemeMode)
+				this.refreshDataContextAction.Reschedule(500);
+		}
 	}
 }
