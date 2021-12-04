a5e0537e518a122c5dd0d113357ce8aeaf424ba6
Hamster <hamster620@gmail.com>
2021-12-04 13:22:47 +0800

    Use DateTimeTextBox in DateTimeSelectionDialog.

--- ULogViewer/Controls/DateTimeSelectionDialog.axaml.cs
+++ ULogViewer/Controls/DateTimeSelectionDialog.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using CarinaStudio.AppSuite.Controls;
+using CarinaStudio.Windows.Input;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,10 +15,7 @@ namespace CarinaStudio.ULogViewer.Controls
     partial class DateTimeSelectionDialog : InputDialog
     {
         // Fields.
-        readonly AppSuite.Controls.DatePicker datePicker;
-        readonly NumericUpDown hoursUpDown;
-        readonly NumericUpDown minutesUpDown;
-        readonly NumericUpDown secondsUpDown;
+        readonly DateTimeTextBox dateTimeTextBox;
 
 
         /// <summary>
@@ -25,19 +24,13 @@ namespace CarinaStudio.ULogViewer.Controls
         public DateTimeSelectionDialog()
         {
             InitializeComponent();
-            this.datePicker = this.FindControl<AppSuite.Controls.DatePicker>(nameof(datePicker));
-            this.hoursUpDown = this.FindControl<NumericUpDown>(nameof(hoursUpDown));
-            this.minutesUpDown = this.FindControl<NumericUpDown>(nameof(minutesUpDown));
-            this.secondsUpDown = this.FindControl<NumericUpDown>(nameof(secondsUpDown));
+            this.dateTimeTextBox = this.FindControl<DateTimeTextBox>(nameof(dateTimeTextBox));
         }
 
 
         // Generate result.
-        protected override Task<object?> GenerateResultAsync(CancellationToken cancellationToken)
-        {
-            var date = this.datePicker.SelectedDate.GetValueOrDefault().Date;
-            return Task.FromResult((object?)new DateTime(date.Year, date.Month, date.Day, (int)this.hoursUpDown.Value, (int)this.minutesUpDown.Value, (int)this.secondsUpDown.Value));
-        }
+        protected override Task<object?> GenerateResultAsync(CancellationToken cancellationToken) =>
+            Task.FromResult((object?)this.dateTimeTextBox.Value.GetValueOrDefault());
 
 
         // Initial date time to show.
@@ -53,8 +46,23 @@ namespace CarinaStudio.ULogViewer.Controls
 
 
         // Selected date changed.
-        void OnDatePickerSelectedDateChanged(object? sender, DatePickerSelectedValueChangedEventArgs e) =>
-            this.InvalidateInput();
+        void OnDateTimeTextBoxPropertyChanged(object? sender, Avalonia.AvaloniaPropertyChangedEventArgs e)
+        {
+            if (e.Property == DateTimeTextBox.IsTextValidProperty || e.Property == DateTimeTextBox.ValueProperty)
+                this.InvalidateInput();
+        }
+
+
+        /// <inheritdoc/>
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            base.OnKeyUp(e);
+            if (e.KeyModifiers == 0 && e.Key == Key.Enter && e.Source == this.dateTimeTextBox && !e.Handled)
+            {
+                e.Handled = true;
+                this.GenerateResultCommand.TryExecute();
+            }
+        }
 
 
         // Dialog opened.
@@ -64,21 +72,18 @@ namespace CarinaStudio.ULogViewer.Controls
             this.FindControl<Avalonia.Controls.TextBlock>("messageTextBlock").AsNonNull().Text = this.Message ?? " ";
             this.InitialDateTime?.Let(it =>
             {
-                this.datePicker.SelectedDate = new DateTimeOffset(it.Date);
-                this.hoursUpDown.Value = it.Hour;
-                this.minutesUpDown.Value = it.Minute;
-                this.secondsUpDown.Value = it.Second;
+                this.dateTimeTextBox.Value = it;
             });
+            this.SynchronizationContext.Post(_ =>
+            {
+                this.dateTimeTextBox.Focus();
+                this.dateTimeTextBox.SelectAll();
+            }, null);
         }
-
-
-        // Selected time changed.
-        void OnTimePickerSelectedTimeChanged(object? sender, TimePickerSelectedValueChangedEventArgs e) =>
-            this.InvalidateInput();
 
 
         // Validate input.
         protected override bool OnValidateInput() =>
-            base.OnValidateInput() && this.datePicker.SelectedDate != null;
+            base.OnValidateInput() && this.dateTimeTextBox.IsTextValid && this.dateTimeTextBox.Value.HasValue;
     }
 }
