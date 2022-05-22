9a42c4237fa845d985b07a3fe9e9e056c95a9625
Hamster <hamster620@gmail.com>
2022-05-22 08:38:59 +0800

    Define product ID.

---
+++ ULogViewer/Products.cs
@@ -0,0 +1,12 @@
+namespace CarinaStudio.ULogViewer;
+
+/// <summary>
+/// ID of ULogViewer products.
+/// </summary>
+static class Products
+{
+    /// <summary>
+    /// Professional version.
+    /// </summary>
+    public const string Professional = "ULogViewer-Pro";
+}
