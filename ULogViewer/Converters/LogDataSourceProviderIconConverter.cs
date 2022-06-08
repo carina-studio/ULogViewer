aaddf761823e103093be4ac974a38322c00671a0
Hamster <hamster620@gmail.com>
2022-06-08 14:38:47 +0800

    Add icons for log data source providers.

---
+++ ULogViewer/Converters/LogDataSourceProviderIconConverter.cs
@@ -0,0 +1,43 @@
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using CarinaStudio.Controls;
+using CarinaStudio.Data.Converters;
+using CarinaStudio.ULogViewer.Logs.DataSources;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace CarinaStudio.ULogViewer.Converters;
+
+/// <summary>
+/// Converter to convert from <see cref="ILogDataSourceProvider"/> to <see cref="IImage"/>.
+/// </summary>
+class LogDataSourceProviderIconConverter : BaseValueConverter<ILogDataSourceProvider?, IImage?>
+{
+    /// <summary>
+    /// Default instance.
+    /// </summary>
+    public static readonly IValueConverter Default = new LogDataSourceProviderIconConverter();
+
+
+    // Constructor.
+    LogDataSourceProviderIconConverter()
+    { }
+
+
+    [return: MaybeNull]
+    protected override IImage Convert(ILogDataSourceProvider? value, object? parameter, CultureInfo culture)
+    {
+        if (value == null)
+            return null;
+        var resName = (parameter as string) == "Light"
+            ? $"Image/LogDataSourceProvider.{value.Name}.Light"
+            : $"Image/LogDataSourceProvider.{value.Name}";
+        if (App.Current.TryFindResource<IImage>(resName, out var icon))
+            return icon;
+        resName = (parameter as string) == "Light"
+            ? $"Image/LogDataSourceProvider.Light"
+            : $"Image/LogDataSourceProvider";
+        App.Current.TryFindResource<IImage>(resName, out icon);
+        return icon;
+    }
+}
