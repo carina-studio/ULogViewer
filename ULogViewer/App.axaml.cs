8878cedd18290e62f6d33e7d959f9d90cbc3c9ef
Hamster <hamster620@gmail.com>
2022-06-21 08:10:46 +0800

    Set release type to Development.

--- ULogViewer/App.axaml.cs
+++ ULogViewer/App.axaml.cs
@@ -471,7 +471,7 @@ namespace CarinaStudio.ULogViewer
 
 
         // Releasing type.
-        public override AppSuite.ApplicationReleasingType ReleasingType => AppSuite.ApplicationReleasingType.Preview;
+        public override AppSuite.ApplicationReleasingType ReleasingType => AppSuite.ApplicationReleasingType.Development;
 
 
 		// Version of settings.
