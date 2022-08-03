1d586c2ed9a7c5fdd3ca113c129045a0c01075c8
Hamster <hamster620@gmail.com>
2022-08-03 14:39:08 +0800

    Support exporting/importing script log data source providers.

--- ULogViewer/Controls/ScriptLogDataSourceProvidersDialog.axaml.cs
+++ ULogViewer/Controls/ScriptLogDataSourceProvidersDialog.axaml.cs
@@ -2,9 +2,11 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using CarinaStudio.AppSuite.Controls;
+using CarinaStudio.Collections;
 using CarinaStudio.Threading;
 using CarinaStudio.ULogViewer.Logs.DataSources;
 using CarinaStudio.ULogViewer.Logs.Profiles;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -83,6 +85,105 @@ partial class ScriptLogDataSourceProvidersDialog : CarinaStudio.Controls.Dialog<
 		}.ShowDialog<ScriptLogDataSourceProvider?>(this);
 		this.providerListBox.SelectedItem = null;
 		this.providerListBox.SelectedItem = provider;
+		this.providerListBox.Focus();
+	}
+
+
+	// Export provider.
+	async void ExportProvider(ScriptLogDataSourceProvider provider)
+	{
+		// select file
+		var fileName = await new SaveFileDialog().Also(dialog =>
+		{
+			dialog.Filters?.Add(new FileDialogFilter().Also(filter =>
+			{
+				filter.Extensions.Add("json");
+				filter.Name = this.Application.GetString("FileFormat.Json");
+			}));
+		}).ShowAsync(this);
+		if (string.IsNullOrEmpty(fileName))
+			return;
+		
+		// export
+		try
+		{
+			await provider.SaveAsync(fileName);
+		}
+		catch (Exception ex)
+		{
+			this.Logger.LogError(ex, $"Failed to export script log data source provider to '{fileName}'");
+			_ = new MessageDialog()
+			{
+				Icon = MessageDialogIcon.Error,
+				Message = new FormattedString().Also(it =>
+				{
+					it.Arg1 = provider.DisplayName;
+					it.Arg2 = fileName;
+					it.Bind(FormattedString.FormatProperty, this.GetResourceObservable("String/ScriptLogDataSourceProvidersDialog.FailedToExportProvider"));
+				})
+			}.ShowDialog(this);
+		}
+	}
+
+
+	// Import provider.
+	async void ImportProvider()
+	{
+		// select file
+		var fileNames = await new OpenFileDialog().Also(dialog =>
+		{
+			dialog.Filters?.Add(new FileDialogFilter().Also(filter =>
+			{
+				filter.Extensions.Add("json");
+				filter.Name = this.Application.GetString("FileFormat.Json");
+			}));
+		}).ShowAsync(this);
+		if (fileNames == null || fileNames.IsEmpty())
+			return;
+		
+		// load provider
+		var provider = await Global.RunOrDefaultAsync(async () =>
+		{
+			return await ScriptLogDataSourceProvider.LoadAsync(fileNames[0]);
+		}, 
+		ex =>
+		{
+			this.Logger.LogError(ex, $"Failed to import script log data source provider from '{fileNames[0]}'");
+			_ = new MessageDialog()
+			{
+				Icon = MessageDialogIcon.Error,
+				Message = new FormattedString().Also(it =>
+				{
+					it.Arg1 = fileNames[0];
+					it.Bind(FormattedString.FormatProperty, this.GetResourceObservable("String/ScriptLogDataSourceProvidersDialog.FailedToImportProvider"));
+				})
+			}.ShowDialog(this);
+		});
+		if (provider == null)
+			return;
+		
+		// select new display name
+		var baseName = BaseNameRegex.Match(provider.DisplayName ?? "").Let(it =>
+			it.Success ? it.Groups["Name"].Value : provider.DisplayName ?? "");
+		var newName = baseName;
+		for (var n = 2; n <= 10; ++n)
+		{
+			var candidateName = $"{baseName} ({n})";
+			if (LogDataSourceProviders.ScriptProviders.FirstOrDefault(it => it.DisplayName == candidateName) == null)
+			{
+				newName = candidateName;
+				break;
+			}
+		}
+		
+		// edit provider and import
+		var newProvider = await new ScriptLogDataSourceProviderEditorDialog()
+		{
+			Provider = new ScriptLogDataSourceProvider(provider, newName),
+		}.ShowDialog<ScriptLogDataSourceProvider?>(this);
+		if (newProvider == null || !LogDataSourceProviders.AddScriptProvider(newProvider))
+			return;
+		this.providerListBox.SelectedItem = newProvider;
 		this.providerListBox.Focus();
 	}
 
