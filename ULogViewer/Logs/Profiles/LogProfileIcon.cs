3840453b5c09e7e6508fd69e05b119a2dc95b8ee
Hamster <hamster620@gmail.com>
2022-09-11 09:06:23 +0800

    Define LogProfileIconColor.

--- ULogViewer/Logs/Profiles/LogProfileIcon.cs
+++ ULogViewer/Logs/Profiles/LogProfileIcon.cs
@@ -1,251 +1,294 @@
 ﻿using System;
 
-namespace CarinaStudio.ULogViewer.Logs.Profiles
+namespace CarinaStudio.ULogViewer.Logs.Profiles;
+
+/// <summary>
+/// Icon of <see cref="LogProfile"/>.
+/// </summary>
+enum LogProfileIcon
 {
 	/// <summary>
-	/// Icon of <see cref="LogProfile"/>.
+	/// Analysis.
 	/// </summary>
-	enum LogProfileIcon
-	{
-		/// <summary>
-		/// Analysis.
-		/// </summary>
-		Analysis,
-		/// <summary>
-		/// Android.
-		/// </summary>
-		Android,
-		/// <summary>
-		/// Apple.
-		/// </summary>
-		Apple,
-		/// <summary>
-		/// Artificial Intelligence (AI).
-		/// </summary>
-		ArtificialIntelligence,
-		/// <summary>
-		/// Audio.
-		/// </summary>
-		Audio,
-		/// <summary>
-		/// AWS.
-		/// </summary>
-		AWS,
-		/// <summary>
-		/// Azure.
-		/// </summary>
-		Azure,
-		/// <summary>
-		/// Battery.
-		/// </summary>
-		Battery,
-		/// <summary>
-		/// Book.
-		/// </summary>
-		Book,
-		/// <summary>
-		/// Checklist.
-		/// </summary>
-		Checklist,
-		/// <summary>
-		/// Cloud data.
-		/// </summary>
-		Cloud,
-		/// <summary>
-		/// Source code.
-		/// </summary>
-		Code,
-		/// <summary>
-		/// Computer.
-		/// </summary>
-		Computer,
-		/// <summary>
-		/// Configuration.
-		/// </summary>
-		Configuration,
-		/// <summary>
-		/// CPU.
-		/// </summary>
-		CPU,
-		/// <summary>
-		/// Database.
-		/// </summary>
-		Database,
-		/// <summary>
-		/// Debug.
-		/// </summary>
-		Debug,
-		/// <summary>
-		/// Diagnostic.
-		/// </summary>
-		Diagnostic,
-		/// <summary>
-		/// Error.
-		/// </summary>
-		Error,
-		/// <summary>
-		/// Experiment.
-		/// </summary>
-		Experiment,
-		/// <summary>
-		/// File.
-		/// </summary>
-		File,
-		/// <summary>
-		/// Flag.
-		/// </summary>
-		Flag,
-		/// <summary>
-		/// Folder.
-		/// </summary>
-		Folder,
-		/// <summary>
-		/// Google Cloud Platform.
-		/// </summary>
-		GCP,
-		/// <summary>
-		/// Image.
-		/// </summary>
-		Image,
-		/// <summary>
-		/// Information.
-		/// </summary>
-		Information,
-		/// <summary>
-		/// Inspiration.
-		/// </summary>
-		Inspiration,
-		/// <summary>
-		/// Layers.
-		/// </summary>
-		Layers,
-		/// <summary>
-		/// Launch.
-		/// </summary>
-		Launch,
-		/// <summary>
-		/// Linux.
-		/// </summary>
-		Linux,
-		/// <summary>
-		/// Linux (Debian).
-		/// </summary>
-		Linux_Debian,
-		/// <summary>
-		/// Linux (Fedora).
-		/// </summary>
-		Linux_Fedora,
-		/// <summary>
-		/// Linux (Open SUSE).
-		/// </summary>
-		Linux_OpenSuse,
-		/// <summary>
-		/// Linux (Mint).
-		/// </summary>
-		Linux_Mint,
-		/// <summary>
-		/// Linux (MX Linux).
-		/// </summary>
-		Linux_MX,
-		/// <summary>
-		/// Linux (Ubuntu).
-		/// </summary>
-		Linux_Ubuntu,
-		/// <summary>
-		/// Location.
-		/// </summary>
-		Location,
-		/// <summary>
-		/// Login.
-		/// </summary>
-		Login,
-		/// <summary>
-		/// Logout.
-		/// </summary>
-		Logout,
-		/// <summary>
-		/// Mark.
-		/// </summary>
-		Mark,
-		/// <summary>
-		/// Network.
-		/// </summary>
-		Network,
-		/// <summary>
-		/// Performance.
-		/// </summary>
-		Performance,
-		/// <summary>
-		/// Smart phone.
-		/// </summary>
-		Phone,
-		/// <summary>
-		/// Power.
-		/// </summary>
-		Power,
-		/// <summary>
-		/// RAM.
-		/// </summary>
-		RAM,
-		/// <summary>
-		/// Screen.
-		/// </summary>
-		Screen,
-		/// <summary>
-		/// Security.
-		/// </summary>
-		Security,
-		/// <summary>
-		/// Server.
-		/// </summary>
-		Server,
-		/// <summary>
-		/// Signal.
-		/// </summary>
-		Signal,
-		/// <summary>
-		/// Statistic.
-		/// </summary>
-		Statistic,
-		/// <summary>
-		/// Table.
-		/// </summary>
-		Table,
-		/// <summary>
-		/// Temperature.
-		/// </summary>
-		Temperature,
-		/// <summary>
-		/// Terminal.
-		/// </summary>
-		Terminal,
-		/// <summary>
-		/// USB.
-		/// </summary>
-		Usb,
-		/// <summary>
-		/// Version control.
-		/// </summary>
-		VersionControl,
-		/// <summary>
-		/// Video.
-		/// </summary>
-		Video,
-		/// <summary>
-		/// Warning.
-		/// </summary>
-		Warning,
-		/// <summary>
-		/// Watch.
-		/// </summary>
-		Watch,
-		/// <summary>
-		/// Website.
-		/// </summary>
-		Website,
-		/// <summary>
-		/// Windows.
-		/// </summary>
-		Windows,
-	}
+	Analysis,
+	/// <summary>
+	/// Android.
+	/// </summary>
+	Android,
+	/// <summary>
+	/// Apple.
+	/// </summary>
+	Apple,
+	/// <summary>
+	/// Artificial Intelligence (AI).
+	/// </summary>
+	ArtificialIntelligence,
+	/// <summary>
+	/// Audio.
+	/// </summary>
+	Audio,
+	/// <summary>
+	/// AWS.
+	/// </summary>
+	AWS,
+	/// <summary>
+	/// Azure.
+	/// </summary>
+	Azure,
+	/// <summary>
+	/// Battery.
+	/// </summary>
+	Battery,
+	/// <summary>
+	/// Book.
+	/// </summary>
+	Book,
+	/// <summary>
+	/// Checklist.
+	/// </summary>
+	Checklist,
+	/// <summary>
+	/// Cloud data.
+	/// </summary>
+	Cloud,
+	/// <summary>
+	/// Source code.
+	/// </summary>
+	Code,
+	/// <summary>
+	/// Computer.
+	/// </summary>
+	Computer,
+	/// <summary>
+	/// Configuration.
+	/// </summary>
+	Configuration,
+	/// <summary>
+	/// CPU.
+	/// </summary>
+	CPU,
+	/// <summary>
+	/// Database.
+	/// </summary>
+	Database,
+	/// <summary>
+	/// Debug.
+	/// </summary>
+	Debug,
+	/// <summary>
+	/// Diagnostic.
+	/// </summary>
+	Diagnostic,
+	/// <summary>
+	/// Error.
+	/// </summary>
+	Error,
+	/// <summary>
+	/// Experiment.
+	/// </summary>
+	Experiment,
+	/// <summary>
+	/// File.
+	/// </summary>
+	File,
+	/// <summary>
+	/// Flag.
+	/// </summary>
+	Flag,
+	/// <summary>
+	/// Folder.
+	/// </summary>
+	Folder,
+	/// <summary>
+	/// Google Cloud Platform.
+	/// </summary>
+	GCP,
+	/// <summary>
+	/// Image.
+	/// </summary>
+	Image,
+	/// <summary>
+	/// Information.
+	/// </summary>
+	Information,
+	/// <summary>
+	/// Inspiration.
+	/// </summary>
+	Inspiration,
+	/// <summary>
+	/// Layers.
+	/// </summary>
+	Layers,
+	/// <summary>
+	/// Launch.
+	/// </summary>
+	Launch,
+	/// <summary>
+	/// Linux.
+	/// </summary>
+	Linux,
+	/// <summary>
+	/// Linux (Debian).
+	/// </summary>
+	Linux_Debian,
+	/// <summary>
+	/// Linux (Fedora).
+	/// </summary>
+	Linux_Fedora,
+	/// <summary>
+	/// Linux (Open SUSE).
+	/// </summary>
+	Linux_OpenSuse,
+	/// <summary>
+	/// Linux (Mint).
+	/// </summary>
+	Linux_Mint,
+	/// <summary>
+	/// Linux (MX Linux).
+	/// </summary>
+	Linux_MX,
+	/// <summary>
+	/// Linux (Ubuntu).
+	/// </summary>
+	Linux_Ubuntu,
+	/// <summary>
+	/// Location.
+	/// </summary>
+	Location,
+	/// <summary>
+	/// Login.
+	/// </summary>
+	Login,
+	/// <summary>
+	/// Logout.
+	/// </summary>
+	Logout,
+	/// <summary>
+	/// Mark.
+	/// </summary>
+	Mark,
+	/// <summary>
+	/// Network.
+	/// </summary>
+	Network,
+	/// <summary>
+	/// Performance.
+	/// </summary>
+	Performance,
+	/// <summary>
+	/// Smart phone.
+	/// </summary>
+	Phone,
+	/// <summary>
+	/// Power.
+	/// </summary>
+	Power,
+	/// <summary>
+	/// RAM.
+	/// </summary>
+	RAM,
+	/// <summary>
+	/// Screen.
+	/// </summary>
+	Screen,
+	/// <summary>
+	/// Security.
+	/// </summary>
+	Security,
+	/// <summary>
+	/// Server.
+	/// </summary>
+	Server,
+	/// <summary>
+	/// Signal.
+	/// </summary>
+	Signal,
+	/// <summary>
+	/// Statistic.
+	/// </summary>
+	Statistic,
+	/// <summary>
+	/// Table.
+	/// </summary>
+	Table,
+	/// <summary>
+	/// Temperature.
+	/// </summary>
+	Temperature,
+	/// <summary>
+	/// Terminal.
+	/// </summary>
+	Terminal,
+	/// <summary>
+	/// USB.
+	/// </summary>
+	Usb,
+	/// <summary>
+	/// Version control.
+	/// </summary>
+	VersionControl,
+	/// <summary>
+	/// Video.
+	/// </summary>
+	Video,
+	/// <summary>
+	/// Warning.
+	/// </summary>
+	Warning,
+	/// <summary>
+	/// Watch.
+	/// </summary>
+	Watch,
+	/// <summary>
+	/// Website.
+	/// </summary>
+	Website,
+	/// <summary>
+	/// Windows.
+	/// </summary>
+	Windows,
+}
+
+
+/// <summary>
+/// Color of log profile icon.
+/// </summary>
+enum LogProfileIconColor
+{
+	/// <summary>
+	/// Default.
+	/// </summary>
+	Default,
+	/// <summary>
+	/// Red.
+	/// </summary>
+	Red,
+	/// <summary>
+	/// Orange.
+	/// </summary>
+	Orange,
+	/// <summary>
+	/// Yellow.
+	/// </summary>
+	Yellow,
+	/// <summary>
+	/// Green.
+	/// </summary>
+	Green,
+	/// <summary>
+	/// Blue.
+	/// </summary>
+	Blue,
+	/// <summary>
+	/// Indigo.
+	/// </summary>
+	Indigo,
+	/// <summary>
+	/// Purple.
+	/// </summary>
+	Purple,
+	/// <summary>
+	/// Magenta.
+	/// </summary>
+	Magenta,
 }
