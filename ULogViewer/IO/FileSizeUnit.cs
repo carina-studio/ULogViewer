51e61dca2121150b1b554b4cb5e6ca44ce6ec9df
Hamster <hamster620@gmail.com>
2022-10-30 14:48:24 +0800

    Define FileSizeUnit and TimeSpanUnit.

---
+++ ULogViewer/IO/FileSizeUnit.cs
@@ -0,0 +1,36 @@
+namespace CarinaStudio.ULogViewer.IO;
+
+/// <summary>
+/// Unit of file size.
+/// </summary>
+enum FileSizeUnit
+{
+    /// <summary>
+    /// Bytes.
+    /// </summary>
+    Bytes,
+    /// <summary>
+    /// Bits.
+    /// </summary>
+    Bits,
+    /// <summary>
+    /// KB.
+    /// </summary>
+    Kilobytes,
+    /// <summary>
+    /// MB.
+    /// </summary>
+    Megabytes,
+    /// <summary>
+    /// GB.
+    /// </summary>
+    Gigabytes,
+    /// <summary>
+    /// TB.
+    /// </summary>
+    Terabytes,
+    /// <summary>
+    /// PB.
+    /// </summary>
+    Petabytes,
+}
---
+++ ULogViewer/TimeSpanUnit.cs
@@ -0,0 +1,36 @@
+namespace CarinaStudio.ULogViewer;
+
+/// <summary>
+/// Unit of time span.
+/// </summary>
+enum TimeSpanUnit
+{
+    /// <summary>
+    /// Milliseconds.
+    /// </summary>
+    Milliseconds,
+    /// <summary>
+    /// Microseconds.
+    /// </summary>
+    Microseconds,
+    /// <summary>
+    /// Nanoseconds.
+    /// </summary>
+    Nanoseconds,
+    /// <summary>
+    /// Seconds.
+    /// </summary>
+    Seconds,
+    /// <summary>
+    /// Minutes.
+    /// </summary>
+    Minutes,
+    /// <summary>
+    /// Hours.
+    /// </summary>
+    Hours,
+    /// <summary>
+    /// Days.
+    /// </summary>
+    Days,
+}
