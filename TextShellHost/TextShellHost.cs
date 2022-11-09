d977336b124109773fffbb2dc00c465e88fdb8d8
Hamster <hamster620@gmail.com>
2022-11-09 09:00:04 +0800

    Add TextShellHost project.

---
+++ TextShellHost/TextShellHost.cs
@@ -0,0 +1,13 @@
+﻿namespace CarinaStudio.ULogViewer;
+
+/// <summary>
+/// Host of text shell.
+/// </summary>
+public static class TextShellHost
+{
+    // Entry point.
+    static void Main(string[] args)
+    {
+        //
+    }
+}
