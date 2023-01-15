90e4a4bfa38a989b007e06acd5a2a3a5f3f8ee9f
Hamster <hamster620@gmail.com>
2023-01-15 20:00:28 +0800

    Detach Session from SessionView when Session removed.

--- ULogViewer/MainWindow.axaml.cs
+++ ULogViewer/MainWindow.axaml.cs
@@ -995,7 +995,11 @@ namespace CarinaStudio.ULogViewer
 									foreach (var token in tokens)
 										token.Dispose();
 								}
+								sessionView.DataContext = null;
 							}
+							if (tabItem.Header is Control header)
+								header.DataContext = null;
+							tabItem.DataContext = null;
 							this.tabItems.RemoveAt(startIndex + i);
 							if (startTime > 0)
 								this.Logger.LogTrace($"Take {this.stopwatch.ElapsedMilliseconds - startTime} ms to remove tab from position {startIndex + i}");
