6a357d82e72ec194b78c48d28e6a2325ce280676
Hamster <hamster620@gmail.com>
2023-01-31 08:49:00 +0800

    [Workaround] Force relayout main window after moving tab.

--- ULogViewer/MainWindow.axaml.cs
+++ ULogViewer/MainWindow.axaml.cs
@@ -718,6 +718,16 @@ namespace CarinaStudio.ULogViewer
 					targetWorkspace.AttachSession(targetIndex, session);
 					targetWorkspace.ActiveSession = session;
 				}
+
+				// [Workaround] Sometimes the content of tab item will gone after moving tab item
+				(this.Content as Control)?.Let(it =>
+				{
+					var margin = it.Margin;
+					it.Margin = new(0, 0, 0, -1);
+					this.SynchronizationContext.Post(() => it.Margin = margin);
+				});
+
+				// complete
 				e.Handled = true;
 				return;
 			}
