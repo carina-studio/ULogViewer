62a54c9356e1b461d96e5466d047df151fb5113e
Hamster <hamster620@gmail.com>
2023-02-26 13:33:49 +0800

    Support searching selected text in log string property dialog on the Internet.

--- ULogViewer/Controls/LogStringPropertyDialog.axaml.cs
+++ ULogViewer/Controls/LogStringPropertyDialog.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Data;
 using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
@@ -10,9 +11,11 @@ using CarinaStudio.Configuration;
 using CarinaStudio.Controls;
 using CarinaStudio.Threading;
 using CarinaStudio.ULogViewer.Converters;
+using CarinaStudio.ULogViewer.Net;
 using CarinaStudio.ULogViewer.ViewModels;
 using CarinaStudio.Windows.Input;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Input;
@@ -25,6 +28,7 @@ namespace CarinaStudio.ULogViewer.Controls
 	partial class LogStringPropertyDialog : AppSuite.Controls.Dialog<IULogViewerApplication>
 	{
 		// Static fields.
+		static readonly StyledProperty<bool> CanSearchOnInternetProperty = AvaloniaProperty.Register<LogStringPropertyDialog, bool>("CanSearchOnInternet", false);
 		static readonly StyledProperty<bool> IsPropertyValueTextEditorFocusedProperty = AvaloniaProperty.Register<LogStringPropertyDialog, bool>(nameof(IsPropertyValueTextEditorFocused), false);
 		static readonly StyledProperty<string> LogPropertyDisplayNameProperty = AvaloniaProperty.Register<LogStringPropertyDialog, string>(nameof(LogPropertyDisplayName), "");
 
@@ -33,6 +37,7 @@ namespace CarinaStudio.ULogViewer.Controls
 		readonly RegexTextBox findTextTextBox;
 		readonly SyntaxHighlightingTextBox propertyValueTextBox;
 		readonly SyntaxHighlightingToken propertyValueHighlightingToken = new();
+		readonly ScheduledAction updateCanSearchOnInternetAction;
 
 
 		/// <summary>
@@ -47,13 +52,56 @@ namespace CarinaStudio.ULogViewer.Controls
 			{
 				it.GetObservable(BoundsProperty).Subscribe(_ =>
 					it.Margin = default);
+				it.GetObservable(TextBox.SelectionStartProperty).Subscribe(() => this.updateCanSearchOnInternetAction?.Schedule());
+				it.GetObservable(TextBox.SelectionEndProperty).Subscribe(() => this.updateCanSearchOnInternetAction?.Schedule());
 				it.DefinitionSet = new SyntaxHighlightingDefinitionSet("Found text").Also(definitionSet =>
 				{
 					this.propertyValueHighlightingToken.Background = this.FindResourceOrDefault<IBrush>("Brush/LogStringPropertyDialog.FoundText.Background", Brushes.LightGray);
 					this.propertyValueHighlightingToken.Foreground = this.FindResourceOrDefault<IBrush>("Brush/LogStringPropertyDialog.FoundText.Foreground", Brushes.Red);
 					definitionSet.TokenDefinitions.Add(this.propertyValueHighlightingToken);
 				});
+				(it.ContextMenu?.Items as IList)?.Let(items =>
+				{
+					foreach (var item in items)
+					{
+						if (item is MenuItem menuItem && menuItem.Name == "searchOnInternetMenuItem")
+						{
+							var providers = Net.SearchProviderManager.Default.Providers;
+							var subItems = new MenuItem[providers.Count];
+							for (var i = 0; i < providers.Count; ++i)
+							{
+								var provider = providers[i];
+								var commandBindingToken = default(IDisposable);
+								var headerBindingToken = default(IDisposable);
+								var iconBindingToken = default(IDisposable);
+								subItems[i] = new MenuItem().Also(subItem =>
+								{
+									var iconImage = new Avalonia.Controls.Image().Also(it =>
+									{
+										it.Classes.Add("MenuItem_Icon");
+									});
+									subItem.CommandParameter = provider;
+									subItem.Icon = iconImage;
+									subItem.AttachedToLogicalTree += (_, _) =>
+									{
+										commandBindingToken = subItem.Bind(MenuItem.CommandProperty, new Binding() { Path = nameof(SearchOnInternet), Source = this });
+										headerBindingToken = subItem.Bind(MenuItem.HeaderProperty, new Binding() { Path = nameof(Net.SearchProvider.Name), Source = provider });
+										iconBindingToken = iconImage.Bind(Avalonia.Controls.Image.SourceProperty, new Binding() { Source = provider, Converter = Converters.SearchProviderIconConverters.Default });
+									};
+									subItem.DetachedFromLogicalTree += (_, _) =>
+									{
+										commandBindingToken = commandBindingToken.DisposeAndReturnNull();
+										headerBindingToken = headerBindingToken.DisposeAndReturnNull();
+										iconBindingToken = iconBindingToken.DisposeAndReturnNull();
+									};
+								});
+							}
+							menuItem.Items = subItems;
+						}
+					}
+				});
 			});
+			this.updateCanSearchOnInternetAction = new(this.UpdateCanSearchOnInternet);
 			this.SetValue(LogPropertyDisplayNameProperty, LogPropertyNameConverter.Default.Convert(nameof(Log.Message)));
 			this.AddHandler(KeyDownEvent, (_, e) =>
 			{
@@ -151,6 +199,25 @@ namespace CarinaStudio.ULogViewer.Controls
 		}
 
 
+		/// <summary>
+		/// Search selected text on the internet.
+		/// </summary>
+		/// <param name="parameter">Search provider.</param>
+		public void SearchOnInternet(object? parameter)
+		{
+			if (parameter is not SearchProvider provider)
+				return;
+			var text = this.propertyValueTextBox.SelectedText?.Trim();
+			if (string.IsNullOrEmpty(text))
+				return;
+			var keywords = new List<string>();
+			foreach (var token in new Text.Tokenizer(text))
+				keywords.Add(new string(token.Value));
+			if (provider.TryCreateSearchUri(keywords, out var uri))
+				Platform.OpenLink(uri);
+		}
+
+
         // set text wrapping.
         void SetTextWrapping(bool wrap)
 		{
@@ -164,6 +231,11 @@ namespace CarinaStudio.ULogViewer.Controls
 
 		// Command to set text wrapping.
 		public ICommand SetTextWrappingCommand { get; }
+
+
+		// Check whether searching on the Internet is available or not.
+		void UpdateCanSearchOnInternet() =>
+			this.SetValue(CanSearchOnInternetProperty, !string.IsNullOrWhiteSpace(this.propertyValueTextBox.SelectedText));
 
 
 		// Update font family of log.
