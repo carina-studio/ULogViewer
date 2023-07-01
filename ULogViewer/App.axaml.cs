345ff5133df65f5a7f7c37927a168bbc5d7485d2
Hamster <hamster620@gmail.com>
2023-07-01 19:51:57 +0800

    Update user agreement to 2.1.

--- ULogViewer/App.axaml.cs
+++ ULogViewer/App.axaml.cs
@@ -792,6 +792,6 @@ namespace CarinaStudio.ULogViewer
 
 
 		/// <inheritdoc/>
-		public override Version UserAgreementVersion => new(2, 0);
+		public override Version UserAgreementVersion => new(2, 1);
     }
 }
