5fd3fa3422023a8ef33c4a408ffc1b3ff30c6c17
Hamster <hamster620@gmail.com>
2024-01-06 17:07:04 +0800

    Support phrase input assistance by PatternEditor and RegexEditorDialog.

--- ULogViewer/Controls/PatternEditor.axaml.cs
+++ ULogViewer/Controls/PatternEditor.axaml.cs
@@ -5,10 +5,15 @@ using Avalonia.Input;
 using Avalonia.LogicalTree;
 using Avalonia.Markup.Xaml;
 using CarinaStudio.AppSuite.Controls;
+using CarinaStudio.Configuration;
 using CarinaStudio.Input.Platform;
 using CarinaStudio.Threading;
+using CarinaStudio.ULogViewer.ViewModels;
 using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CarinaStudio.ULogViewer.Controls;
 
@@ -30,6 +35,10 @@ class PatternEditor : CarinaStudio.Controls.UserControl<IULogViewerApplication>
 	/// </summary>
 	public static readonly DirectProperty<PatternEditor, bool> IsPatternTextValidProperty = AvaloniaProperty.RegisterDirect<PatternEditor, bool>(nameof(IsPatternTextValid), e => e.isPatternTextValid);
 	/// <summary>
+	/// Property of <see cref="IsPhraseInputAssistanceEnabled"/>.
+	/// </summary>
+	public static readonly DirectProperty<PatternEditor, bool> IsPhraseInputAssistanceEnabledProperty = AvaloniaProperty.RegisterDirect<PatternEditor, bool>(nameof(IsPhraseInputAssistanceEnabled), e => e.isPhraseInputAssistanceEnabled, (e, isEnabled) => e.IsPhraseInputAssistanceEnabled = isEnabled);
+	/// <summary>
 	/// Property of <see cref="Pattern"/>.
 	/// </summary>
 	public static readonly DirectProperty<PatternEditor, Regex?> PatternProperty = AvaloniaProperty.RegisterDirect<PatternEditor, Regex?>(nameof(Pattern), e => e.pattern, (e, p) => e.Pattern = p);
@@ -37,6 +46,14 @@ class PatternEditor : CarinaStudio.Controls.UserControl<IULogViewerApplication>
 	/// Property of <see cref="Watermark"/>.
 	/// </summary>
 	public static readonly StyledProperty<string?> WatermarkProperty = AvaloniaProperty.Register<PatternEditor, string?>(nameof(Watermark));
+	
+	
+	// Provider for assistance of phrase input.
+	class PhraseInputAssistanceProvider : IPhraseInputAssistanceProvider
+	{
+		public Task<IList<string>> SelectCandidatePhrasesAsync(string prefix, string? postfix, CancellationToken cancellationToken) =>
+			LogTextFilterPhrasesDatabase.SelectCandidatePhrasesAsync(prefix, postfix, cancellationToken);
+	}
 
 
 	// Constants.
@@ -47,8 +64,11 @@ class PatternEditor : CarinaStudio.Controls.UserControl<IULogViewerApplication>
 	readonly Button editPatternButton;
 	bool hasDialog;
 	bool isPatternTextValid = true;
+	bool isPhraseInputAssistanceEnabled;
 	Regex? pattern;
 	readonly RegexTextBox patternTextBox;
+	readonly ScheduledAction updatePhrasesDatabaseAction;
+	// ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
 	readonly ScheduledAction updatePredefinedCapturingGroupsAction;
 	Avalonia.Controls.Window? window;
 
@@ -62,6 +82,13 @@ class PatternEditor : CarinaStudio.Controls.UserControl<IULogViewerApplication>
 		this.editPatternButton = this.Get<Button>(nameof(editPatternButton));
 		this.patternTextBox = this.Get<RegexTextBox>(nameof(patternTextBox)).Also(it =>
 		{
+			it.GetObservable(RegexTextBox.HasOpenedAssistanceMenusProperty).Subscribe(hasOpenedMenus =>
+			{
+				if (hasOpenedMenus)
+					this.updatePhrasesDatabaseAction?.Cancel();
+				else if (this.isPhraseInputAssistanceEnabled)
+					this.updatePhrasesDatabaseAction?.Reschedule(this.Application.Configuration.GetValueOrDefault(ConfigurationKeys.LogTextFilterPhrasesDatabaseUpdateDelay));
+			});
 			it.GetObservable(RegexTextBox.IsTextValidProperty).Subscribe(isValid =>
 			{
 				this.SetAndRaise(IsPatternTextValidProperty, ref this.isPatternTextValid, isValid);
@@ -70,7 +97,19 @@ class PatternEditor : CarinaStudio.Controls.UserControl<IULogViewerApplication>
 			it.GetObservable(RegexTextBox.ObjectProperty).Subscribe(pattern =>
 			{
 				this.SetAndRaise(PatternProperty, ref this.pattern, pattern);
+				if (this.isPhraseInputAssistanceEnabled)
+					this.updatePhrasesDatabaseAction?.Reschedule(this.Application.Configuration.GetValueOrDefault(ConfigurationKeys.LogTextFilterPhrasesDatabaseUpdateDelay));
 			});
+		});
+		this.updatePhrasesDatabaseAction = new(() =>
+		{
+			if (this.window is null 
+			    || !this.isPhraseInputAssistanceEnabled 
+			    || this.patternTextBox.HasOpenedAssistanceMenus)
+			{
+				return;
+			}
+			this.patternTextBox.Object?.Let(it => LogTextFilterPhrasesDatabase.UpdatePhrasesAsync(it, default));
 		});
 		this.updatePredefinedCapturingGroupsAction = new(() =>
 		{
@@ -134,17 +173,21 @@ class PatternEditor : CarinaStudio.Controls.UserControl<IULogViewerApplication>
 	{
 		// check state
 		this.VerifyAccess();
-		if (this.window == null || this.hasDialog)
+		if (this.window is null || this.hasDialog)
 			return;
+		
+		// update phrases database
+		this.updatePhrasesDatabaseAction.ExecuteIfScheduled();
 		
 		// edit
 		this.hasDialog = true;
-		var regex = await new RegexEditorDialog()
+		var regex = await new RegexEditorDialog
 		{
 			IgnoreCase = this.patternTextBox.IgnoreCase,
 			InitialRegexText = this.patternTextBox.Text,
 			IsCapturingGroupsEnabled = this.GetValue(IsCapturingGroupsEnabledProperty),
 			IsCapturingLogPropertiesEnabled = this.GetValue(IsCapturingLogPropertiesEnabledProperty),
+			IsPhraseInputAssistanceEnabled = this.isPhraseInputAssistanceEnabled,
 		}.ShowDialog<Regex?>(this.window);
 		this.hasDialog = false;
 		if (regex == null)
@@ -156,6 +199,7 @@ class PatternEditor : CarinaStudio.Controls.UserControl<IULogViewerApplication>
 		// update pattern
 		this.patternTextBox.IgnoreCase = (regex.Options & RegexOptions.IgnoreCase) != 0;
 		this.patternTextBox.Object = regex;
+		this.updatePhrasesDatabaseAction.Cancel();
 		this.SetAndRaise(PatternProperty, ref this.pattern, regex);
 		this.patternTextBox.Focus();
 	}
@@ -186,6 +230,33 @@ class PatternEditor : CarinaStudio.Controls.UserControl<IULogViewerApplication>
 	/// </summary>
 	public bool IsPatternTextValid => this.isPatternTextValid;
 	
+	
+	/// <summary>
+	/// Get or set whether assistance of phrase input is enabled or not.
+	/// </summary>
+	public bool IsPhraseInputAssistanceEnabled
+	{
+		get => this.isPhraseInputAssistanceEnabled;
+		set
+		{
+			this.VerifyAccess();
+			if (this.isPhraseInputAssistanceEnabled == value)
+				return;
+			if (value)
+			{
+				this.patternTextBox.PhraseInputAssistanceProvider = new PhraseInputAssistanceProvider();
+				if (!this.patternTextBox.HasOpenedAssistanceMenus)
+					this.updatePhrasesDatabaseAction.Schedule();
+			}
+			else
+			{
+				this.patternTextBox.PhraseInputAssistanceProvider = null;
+				this.updatePhrasesDatabaseAction.Cancel();
+			}
+			this.SetAndRaise(IsPhraseInputAssistanceEnabledProperty, ref this.isPhraseInputAssistanceEnabled, value);
+		}
+	}
+	
 
 	/// <inheritdoc/>
 	protected override void OnAttachedToLogicalTree(LogicalTreeAttachmentEventArgs e)
@@ -198,6 +269,7 @@ class PatternEditor : CarinaStudio.Controls.UserControl<IULogViewerApplication>
 	/// <inheritdoc/>
 	protected override void OnDetachedFromLogicalTree(LogicalTreeAttachmentEventArgs e)
 	{
+		this.updatePhrasesDatabaseAction.ExecuteIfScheduled();
 		this.window = null;
 		base.OnDetachedFromLogicalTree(e);
 	}
@@ -219,12 +291,12 @@ class PatternEditor : CarinaStudio.Controls.UserControl<IULogViewerApplication>
 	{
 		// check state
 		this.VerifyAccess();
-		if (this.window == null || this.hasDialog)
+		if (this.window is null || this.hasDialog)
 			return;
 		
 		// get data from clipboard
 		var clipboard = TopLevel.GetTopLevel(this)?.Clipboard;
-		if (clipboard == null)
+		if (clipboard is null)
 			return;
         var text = await clipboard.GetTextAsync();
         if (string.IsNullOrEmpty(text))
@@ -241,10 +313,10 @@ class PatternEditor : CarinaStudio.Controls.UserControl<IULogViewerApplication>
         catch
         { }
         // confirm replacing current pattern
-        if (this.pattern != null)
+        if (this.pattern is not null)
         {
 			this.hasDialog = true;
-            var result = await new MessageDialog()
+            var result = await new MessageDialog
             {
                 Buttons = MessageDialogButtons.YesNo,
                 Icon = MessageDialogIcon.Question,
@@ -284,6 +356,7 @@ class PatternEditor : CarinaStudio.Controls.UserControl<IULogViewerApplication>
 			if (value != null)
 				this.patternTextBox.IgnoreCase = (value.Options & RegexOptions.IgnoreCase) != 0;
 			this.patternTextBox.Object = value;
+			this.updatePhrasesDatabaseAction.Cancel();
 			this.SetAndRaise(PatternProperty, ref this.pattern, value);
 		}
 	}
--- ULogViewer/Controls/RegexEditorDialog.axaml.cs
+++ ULogViewer/Controls/RegexEditorDialog.axaml.cs
@@ -4,8 +4,10 @@ using Avalonia.Data;
 using Avalonia.Markup.Xaml;
 using CarinaStudio.AppSuite.Controls;
 using CarinaStudio.Collections;
+using CarinaStudio.Configuration;
 using CarinaStudio.Threading;
 using CarinaStudio.ULogViewer.Logs;
+using CarinaStudio.ULogViewer.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,197 +15,265 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace CarinaStudio.ULogViewer.Controls
+namespace CarinaStudio.ULogViewer.Controls;
+
+/// <summary>
+/// Editor of <see cref="Regex"/>.
+/// </summary>
+class RegexEditorDialog : InputDialog<IULogViewerApplication>
 {
 	/// <summary>
-	/// Editor of <see cref="Regex"/>.
+	/// List of property names of log.
 	/// </summary>
-	class RegexEditorDialog : InputDialog<IULogViewerApplication>
+	public static readonly IList<string> LogPropertyNames = Log.PropertyNames.Where(it =>
 	{
-		/// <summary>
-		/// List of property names of log.
-		/// </summary>
-		public static readonly IList<string> LogPropertyNames = Log.PropertyNames.Where(it =>
+		return it switch
 		{
-			return it switch
-			{
-				nameof(Log.FileName) => false,
-				nameof(Log.LineNumber) => false,
-				_ => true,
-			};
-		}).ToList().AsReadOnly();
+			nameof(Log.FileName) => false,
+			nameof(Log.LineNumber) => false,
+			_ => true,
+		};
+	}).ToList().AsReadOnly();
 
 
-		// Static fields.
-		static readonly Regex DefaultRegexGroupNameRegex = new("^[\\d]+$");
-		static readonly StyledProperty<bool> HasTestResultProperty = AvaloniaProperty.Register<RegexEditorDialog, bool>("HasTestResult");
-		static readonly StyledProperty<bool> IsCapturingGroupsEnabledProperty = AvaloniaProperty.Register<RegexEditorDialog, bool>(nameof(IsCapturingGroupsEnabled));
-		static readonly StyledProperty<bool> IsCapturingLogPropertiesEnabledProperty = AvaloniaProperty.Register<RegexEditorDialog, bool>(nameof(IsCapturingLogPropertiesEnabled));
-		static readonly StyledProperty<string?> TestLogLineProperty = AvaloniaProperty.Register<RegexEditorDialog, string?>("TestLogLine");
-		static readonly StyledProperty<bool> TestResultProperty = AvaloniaProperty.Register<RegexEditorDialog, bool>("TestResult");
-
-
-		// Fields.
-		readonly ObservableList<Tuple<string, string>> capturedGroups = new();
-		readonly RegexTextBox regexTextBox;
-		readonly ScheduledAction testAction;
-		readonly TextBox testLogLineTextBox;
-
-
-		/// <summary>
-		/// Initialize new <see cref="RegexEditorDialog"/> instance.
-		/// </summary>
-		public RegexEditorDialog()
-		{
-			this.CapturedGroups = ListExtensions.AsReadOnly(this.capturedGroups);
-			AvaloniaXamlLoader.Load(this);
-			this.regexTextBox = this.FindControl<RegexTextBox>(nameof(regexTextBox))!.Also(it =>
-			{
-				it.GetObservable(RegexTextBox.IsTextValidProperty).Subscribe(_ =>
-				{
-					this.InvalidateInput();
-					this.testAction?.Schedule();
-				});
-				it.GetObservable(RegexTextBox.ObjectProperty).Subscribe(_ =>
-				{
-					this.InvalidateInput();
-					this.testAction?.Schedule();
-				});
-			});
-			this.testAction = new ScheduledAction(() =>
-			{
-				var regex = this.regexTextBox.Object;
-				var testLine = this.GetValue(TestLogLineProperty);
-				this.capturedGroups.Clear();
-				if (!this.regexTextBox.IsTextValid || regex == null || string.IsNullOrEmpty(testLine))
-				{
-					this.SetValue(HasTestResultProperty, false);
-					return;
-				}
-				var match = regex.Match(testLine);
-				if (match.Success)
-				{
-					this.SetValue(TestResultProperty, true);
-					foreach (var name in match.Groups.Keys)
-                    {
-						var group = match.Groups[name];
-						if (DefaultRegexGroupNameRegex.IsMatch(name))
-							continue;
-						this.capturedGroups.Add(new(name, group.Value));
-                    }
-				}
-				else
-					this.SetValue(TestResultProperty, false);
-				this.SetValue(HasTestResultProperty, true);
-			});
-			this.testLogLineTextBox = this.FindControl<TextBox>(nameof(testLogLineTextBox)).AsNonNull();
-		}
-
-
-		/// <summary>
-		/// List of captured groups.
-		/// </summary>
-		public IList<Tuple<string, string>> CapturedGroups { get; }
-
-
-		// Generate result.
-		protected override Task<object?> GenerateResultAsync(CancellationToken cancellationToken) =>
-			Task.FromResult<object?>(this.regexTextBox.Object);
-		
-
-		// Ignore case of pattern.
-		public bool? IgnoreCase { get; set; }
-
-
-		// Initial Regex.
-		public Regex? InitialRegex { get; set; }
-
-
-		// Initial text of regex.
-		public string? InitialRegexText { get; set; }
-
-
-		// Whether group capturing is enabled or not.
-		public bool IsCapturingGroupsEnabled
-		{
-			get => this.GetValue(IsCapturingGroupsEnabledProperty);
-			set => this.SetValue(IsCapturingGroupsEnabledProperty, value);
-		}
-
-
-		// Whether log property capturing is enabled or not.
-		public bool IsCapturingLogPropertiesEnabled
-		{
-			get => this.GetValue(IsCapturingLogPropertiesEnabledProperty);
-			set => this.SetValue(IsCapturingLogPropertiesEnabledProperty, value);
-		}
-
-
-		/// <inheritdoc/>
-		protected override void OnOpened(EventArgs e)
-		{
-			base.OnOpened(e);
-			this.SynchronizationContext.Post(() => this.regexTextBox.Focus());
-		}
-
-
-		/// <inheritdoc/>
-		protected override void OnOpening(EventArgs e)
-		{
-			base.OnOpening(e);
-			var testLogLineDescriptionTextBlock = this.Get<TextBlock>("testLogLineDescriptionTextBlock");
-			if (this.IsCapturingGroupsEnabled)
-			{
-				if (this.IsCapturingLogPropertiesEnabled)
-				{
-					foreach (var propertyName in LogPropertyNames)
-					{
-						var group = new RegexGroup().Also(group =>
-						{
-							group.Bind(RegexGroup.DisplayNameProperty, new Binding()
-							{
-								Converter = Converters.LogPropertyNameConverter.Default,
-								Path = nameof(RegexGroup.Name),
-								Source = group,
-							});
-							group.Name = propertyName;
-						});
-						this.regexTextBox.PredefinedGroups.Add(group);
-					}
-					testLogLineDescriptionTextBlock.Bind(TextBlock.TextProperty, this.Application.GetObservableString("RegexEditorDialog.TestLogLine.Description.CapturingLogProperties"));
-				}
-				else
-					testLogLineDescriptionTextBlock.Bind(TextBlock.TextProperty, this.Application.GetObservableString("RegexEditorDialog.TestLogLine.Description.CapturingGroups"));
-			}
-			else
-				testLogLineDescriptionTextBlock.Bind(TextBlock.TextProperty, this.Application.GetObservableString("RegexEditorDialog.TestLogLine.Description"));
-			var regex = this.InitialRegex;
-			if (regex != null)
-			{
-				this.regexTextBox.IgnoreCase = this.IgnoreCase ?? (regex.Options & RegexOptions.IgnoreCase) != 0;
-				this.regexTextBox.Object = regex;
-			}
-			else if (this.InitialRegexText != null)
-			{
-				this.regexTextBox.IgnoreCase = this.IgnoreCase ?? true;
-				this.regexTextBox.Text = this.InitialRegexText;
-			}
-			else
-				this.regexTextBox.IgnoreCase = this.IgnoreCase ?? true;
-		}
-
-
-		// Property changed.
-        protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
-        {
-            base.OnPropertyChanged(change);
-			if (change.Property == TestLogLineProperty)
-				this.testAction.Schedule();
-        }
-
-
-		// Validate input.
-		protected override bool OnValidateInput() =>
-			base.OnValidateInput() && this.regexTextBox.IsTextValid && this.regexTextBox.Object != null;
+	// Provider for assistance of phrase input.
+	class PhraseInputAssistanceProvider : IPhraseInputAssistanceProvider
+	{
+		public Task<IList<string>> SelectCandidatePhrasesAsync(string prefix, string? postfix, CancellationToken cancellationToken) =>
+			LogTextFilterPhrasesDatabase.SelectCandidatePhrasesAsync(prefix, postfix, cancellationToken);
 	}
+
+
+	// Static fields.
+	static readonly Regex DefaultRegexGroupNameRegex = new("^[\\d]+$");
+	static readonly StyledProperty<bool> HasTestResultProperty = AvaloniaProperty.Register<RegexEditorDialog, bool>("HasTestResult");
+	static readonly StyledProperty<bool> IsCapturingGroupsEnabledProperty = AvaloniaProperty.Register<RegexEditorDialog, bool>(nameof(IsCapturingGroupsEnabled));
+	static readonly StyledProperty<bool> IsCapturingLogPropertiesEnabledProperty = AvaloniaProperty.Register<RegexEditorDialog, bool>(nameof(IsCapturingLogPropertiesEnabled));
+	static readonly StyledProperty<string?> TestLogLineProperty = AvaloniaProperty.Register<RegexEditorDialog, string?>("TestLogLine");
+	static readonly StyledProperty<bool> TestResultProperty = AvaloniaProperty.Register<RegexEditorDialog, bool>("TestResult");
+
+
+	// Fields.
+	readonly ObservableList<Tuple<string, string>> capturedGroups = new();
+	bool isPhraseInputAssistanceEnabled;
+	readonly RegexTextBox regexTextBox;
+	readonly ScheduledAction testAction;
+	readonly TextBox testLogLineTextBox;
+	readonly ScheduledAction updatePhrasesDatabaseAction;
+
+
+	/// <summary>
+	/// Initialize new <see cref="RegexEditorDialog"/> instance.
+	/// </summary>
+	public RegexEditorDialog()
+	{
+		this.CapturedGroups = ListExtensions.AsReadOnly(this.capturedGroups);
+		AvaloniaXamlLoader.Load(this);
+		this.regexTextBox = this.FindControl<RegexTextBox>(nameof(regexTextBox))!.Also(it =>
+		{
+			it.GetObservable(RegexTextBox.HasOpenedAssistanceMenusProperty).Subscribe(hasOpenedMenus =>
+			{
+				if (hasOpenedMenus)
+					this.updatePhrasesDatabaseAction?.Cancel();
+				else if (this.isPhraseInputAssistanceEnabled)
+					this.updatePhrasesDatabaseAction?.Reschedule(this.Application.Configuration.GetValueOrDefault(ConfigurationKeys.LogTextFilterPhrasesDatabaseUpdateDelay));
+			});
+			it.GetObservable(RegexTextBox.IsTextValidProperty).Subscribe(_ =>
+			{
+				this.InvalidateInput();
+				this.testAction?.Schedule();
+			});
+			it.GetObservable(RegexTextBox.ObjectProperty).Subscribe(_ =>
+			{
+				this.InvalidateInput();
+				this.testAction?.Schedule();
+				if (this.isPhraseInputAssistanceEnabled)
+					this.updatePhrasesDatabaseAction?.Reschedule(this.Application.Configuration.GetValueOrDefault(ConfigurationKeys.LogTextFilterPhrasesDatabaseUpdateDelay));
+			});
+		});
+		this.testAction = new ScheduledAction(() =>
+		{
+			var regex = this.regexTextBox.Object;
+			var testLine = this.GetValue(TestLogLineProperty);
+			this.capturedGroups.Clear();
+			if (!this.regexTextBox.IsTextValid || regex == null || string.IsNullOrEmpty(testLine))
+			{
+				this.SetValue(HasTestResultProperty, false);
+				return;
+			}
+			var match = regex.Match(testLine);
+			if (match.Success)
+			{
+				this.SetValue(TestResultProperty, true);
+				foreach (var name in match.Groups.Keys)
+                {
+					var group = match.Groups[name];
+					if (DefaultRegexGroupNameRegex.IsMatch(name))
+						continue;
+					this.capturedGroups.Add(new(name, group.Value));
+                }
+			}
+			else
+				this.SetValue(TestResultProperty, false);
+			this.SetValue(HasTestResultProperty, true);
+		});
+		this.testLogLineTextBox = this.FindControl<TextBox>(nameof(testLogLineTextBox)).AsNonNull();
+		this.updatePhrasesDatabaseAction = new(() =>
+		{
+			if (!this.IsOpened 
+			    || !this.isPhraseInputAssistanceEnabled 
+			    || this.regexTextBox.HasOpenedAssistanceMenus)
+			{
+				return;
+			}
+			this.regexTextBox.Object?.Let(it => LogTextFilterPhrasesDatabase.UpdatePhrasesAsync(it, default));
+		});
+	}
+
+
+	/// <summary>
+	/// List of captured groups.
+	/// </summary>
+	public IList<Tuple<string, string>> CapturedGroups { get; }
+
+
+	// Generate result.
+	protected override Task<object?> GenerateResultAsync(CancellationToken cancellationToken)
+	{
+		this.updatePhrasesDatabaseAction.ExecuteIfScheduled();
+		return Task.FromResult<object?>(this.regexTextBox.Object);
+	}
+	
+
+	// Ignore case of pattern.
+	public bool? IgnoreCase { get; set; }
+
+
+	// Initial Regex.
+	public Regex? InitialRegex { get; set; }
+
+
+	// Initial text of regex.
+	public string? InitialRegexText { get; set; }
+
+
+	// Whether group capturing is enabled or not.
+	public bool IsCapturingGroupsEnabled
+	{
+		get => this.GetValue(IsCapturingGroupsEnabledProperty);
+		set => this.SetValue(IsCapturingGroupsEnabledProperty, value);
+	}
+
+
+	// Whether log property capturing is enabled or not.
+	public bool IsCapturingLogPropertiesEnabled
+	{
+		get => this.GetValue(IsCapturingLogPropertiesEnabledProperty);
+		set => this.SetValue(IsCapturingLogPropertiesEnabledProperty, value);
+	}
+
+
+	/// <summary>
+	/// Get or set whether assistance of phrase input is enabled or not.
+	/// </summary>
+	public bool IsPhraseInputAssistanceEnabled
+	{
+		get => this.isPhraseInputAssistanceEnabled;
+		set
+		{
+			this.VerifyAccess();
+			if (this.isPhraseInputAssistanceEnabled == value)
+				return;
+			if (value)
+			{
+				this.regexTextBox.PhraseInputAssistanceProvider = new PhraseInputAssistanceProvider();
+				if (!this.regexTextBox.HasOpenedAssistanceMenus)
+					this.updatePhrasesDatabaseAction.Schedule();
+			}
+			else
+			{
+				this.regexTextBox.PhraseInputAssistanceProvider = null;
+				this.updatePhrasesDatabaseAction.Cancel();
+			}
+			this.isPhraseInputAssistanceEnabled = value;
+		}
+	}
+
+
+	/// <inheritdoc/>
+	protected override void OnClosed(EventArgs e)
+	{
+		this.updatePhrasesDatabaseAction.Cancel();
+		base.OnClosed(e);
+	}
+
+
+	/// <inheritdoc/>
+	protected override void OnOpened(EventArgs e)
+	{
+		base.OnOpened(e);
+		this.SynchronizationContext.Post(() => this.regexTextBox.Focus());
+	}
+
+
+	/// <inheritdoc/>
+	protected override void OnOpening(EventArgs e)
+	{
+		base.OnOpening(e);
+		var testLogLineDescriptionTextBlock = this.Get<TextBlock>("testLogLineDescriptionTextBlock");
+		if (this.IsCapturingGroupsEnabled)
+		{
+			if (this.IsCapturingLogPropertiesEnabled)
+			{
+				foreach (var propertyName in LogPropertyNames)
+				{
+					var group = new RegexGroup().Also(group =>
+					{
+						group.Bind(RegexGroup.DisplayNameProperty, new Binding()
+						{
+							Converter = Converters.LogPropertyNameConverter.Default,
+							Path = nameof(RegexGroup.Name),
+							Source = group,
+						});
+						group.Name = propertyName;
+					});
+					this.regexTextBox.PredefinedGroups.Add(group);
+				}
+				testLogLineDescriptionTextBlock.Bind(TextBlock.TextProperty, this.Application.GetObservableString("RegexEditorDialog.TestLogLine.Description.CapturingLogProperties"));
+			}
+			else
+				testLogLineDescriptionTextBlock.Bind(TextBlock.TextProperty, this.Application.GetObservableString("RegexEditorDialog.TestLogLine.Description.CapturingGroups"));
+		}
+		else
+			testLogLineDescriptionTextBlock.Bind(TextBlock.TextProperty, this.Application.GetObservableString("RegexEditorDialog.TestLogLine.Description"));
+		var regex = this.InitialRegex;
+		if (regex is not null)
+		{
+			this.regexTextBox.IgnoreCase = this.IgnoreCase ?? (regex.Options & RegexOptions.IgnoreCase) != 0;
+			this.regexTextBox.Object = regex;
+		}
+		else if (this.InitialRegexText is not null)
+		{
+			this.regexTextBox.IgnoreCase = this.IgnoreCase ?? true;
+			this.regexTextBox.Text = this.InitialRegexText;
+			this.regexTextBox.Validate();
+		}
+		else
+			this.regexTextBox.IgnoreCase = this.IgnoreCase ?? true;
+		this.updatePhrasesDatabaseAction.Cancel();
+	}
+
+
+	// Property changed.
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+		if (change.Property == TestLogLineProperty)
+			this.testAction.Schedule();
+    }
+
+
+	// Validate input.
+	protected override bool OnValidateInput() =>
+		base.OnValidateInput() && this.regexTextBox.IsTextValid && this.regexTextBox.Object != null;
 }
