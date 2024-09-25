0370cb0444fb352a40ddf873cdb02e8ca85c05f4
Hamster <hamster620@gmail.com>
2024-09-25 20:28:11 +0800

    Add LogDataSourceOptions.FormatClefData.

--- ULogViewer/Logs/DataSources/ILogDataSourceProvider.cs
+++ ULogViewer/Logs/DataSources/ILogDataSourceProvider.cs
@@ -200,6 +200,7 @@ namespace CarinaStudio.ULogViewer.Logs.DataSources
 			&& Equals(this.Encoding, options.Encoding)
 			&& this.EnvironmentVariables.Equals(options.EnvironmentVariables)
 			&& this.FileName == options.FileName
+			&& this.FormatClefData == options.FormatClefData
 			&& this.FormatJsonData == options.FormatJsonData
 			&& this.FormatXmlData == options.FormatXmlData
 			&& this.IncludeStandardError == options.IncludeStandardError
@@ -226,6 +227,12 @@ namespace CarinaStudio.ULogViewer.Logs.DataSources
 		/// Get or set name of file to open.
 		/// </summary>
 		public string? FileName { get; set; }
+		
+		
+		/// <summary>
+		/// Get or set whether CLEF data should be formatted when reading or not.
+		/// </summary>
+		public bool FormatClefData { get; set; }
 
 
 		/// <summary>
@@ -382,6 +389,9 @@ namespace CarinaStudio.ULogViewer.Logs.DataSources
 						case nameof(FileName):
 							options.FileName = jsonProperty.Value.GetString();
 							break;
+						case nameof(FormatClefData):
+							options.FormatClefData = jsonProperty.Value.ValueKind == JsonValueKind.True;
+							break;
 						case nameof(FormatJsonData):
 							options.FormatJsonData = jsonProperty.Value.ValueKind == JsonValueKind.True;
 							break;
@@ -534,6 +544,8 @@ namespace CarinaStudio.ULogViewer.Logs.DataSources
 				});
 				this.FileName?.Let(it => 
 					jsonWriter.WriteString(nameof(FileName), it));
+				if (this.FormatClefData)
+					jsonWriter.WriteBoolean(nameof(FormatClefData), true);
 				if (this.FormatJsonData)
 					jsonWriter.WriteBoolean(nameof(FormatJsonData), true);
 				if (this.FormatXmlData)
