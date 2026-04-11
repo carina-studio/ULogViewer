39810ba3d484b5dee5462d7dea9fe977b4f73949
Carina <hamster620@gmail.com>
2026-04-11 09:16:00 +0800

    Remove URIs for checking application update.

--- ULogViewer/App.axaml.cs
+++ ULogViewer/App.axaml.cs
@@ -752,20 +752,7 @@ namespace CarinaStudio.ULogViewer
 
 
 		// URI of package manifest.
-		public override IEnumerable<Uri> PackageManifestUris => !this.Settings.GetValueOrDefault(AppSuite.SettingKeys.AcceptNonStableApplicationUpdate)
-			? new[]{ Uris.AppPackageManifest }
-			: this.ReleasingType == ApplicationReleasingType.Development
-				? new[]
-				{
-					Uris.DevelopmentAppPackageManifest,
-					Uris.PreviewAppPackageManifest,
-					Uris.AppPackageManifest,
-				} 
-				: new[]
-				{
-					Uris.PreviewAppPackageManifest,
-					Uris.AppPackageManifest,
-				};
+		public override IEnumerable<Uri> PackageManifestUris => [ ];
 
 
 		/// <inheritdoc/>
