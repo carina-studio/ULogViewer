47c66a08e22c8a79ed174a776745dccc70eb27a6
Carina <hamster620@gmail.com>
2026-04-14 11:10:10 +0800

    [1] Upgrade to AppSuite 3.0.5.413-preview. [2] Update strings for usage data collection confirmation.

--- ULogViewer/ViewModels/AppInfo.cs
+++ ULogViewer/ViewModels/AppInfo.cs
@@ -2,35 +2,32 @@
 using System;
 using System.Collections.Generic;
 
-namespace CarinaStudio.ULogViewer.ViewModels
+namespace CarinaStudio.ULogViewer.ViewModels;
+
+/// <summary>
+/// Application information.
+/// </summary>
+class AppInfo : AppSuite.ViewModels.ApplicationInfo
 {
-    /// <summary>
-    /// Application information.
-    /// </summary>
-    class AppInfo : AppSuite.ViewModels.ApplicationInfo
+    // Constructor.
+    public AppInfo()
     {
-        // Constructor.
-        public AppInfo()
-        {
-            var isProVersion = this.Application.ProductManager.Let(it =>
-                !it.IsMock && it.IsProductActivated(ULogViewer.Products.Professional));
-            if (isProVersion)
-                this.Badges = new[] { this.Application.FindResourceOrDefault<IImage?>("Image/Icon.Professional").AsNonNull() };
-            else
-                this.Badges = Array.Empty<IImage>();
-        }
-
-
-        // Badges.
-        public override IList<IImage> Badges { get; }
-
-
-        // URI of GitHub project.
-        public override Uri GitHubProjectUri => new("https://github.com/carina-studio/ULogViewer");
-
-
-        // URI of website.
-        public override Uri WebsiteUri =>
-            new($"https://carinastudio.azurewebsites.net/ULogViewer/");
+        var isProVersion = this.Application.ProductManager.Let(it =>
+            !it.IsMock && it.IsProductActivated(ULogViewer.Products.Professional));
+        this.Badges = isProVersion 
+            ? [ this.Application.FindResourceOrDefault<IImage?>("Image/Icon.Professional").AsNonNull() ]
+            : [ ];
     }
+
+
+    // Badges.
+    public override IList<IImage> Badges { get; }
+
+
+    // URI of GitHub project.
+    public override Uri GitHubProjectUri => new("https://github.com/carina-studio/ULogViewer");
+    
+
+    // URI of website.
+    public override Uri WebsiteUri => new("https://carinastudio.azurewebsites.net/ULogViewer/");
 }
